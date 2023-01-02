cff7c39bb8745715296e7a0742b2833e63543c03
Roman <11-roman@mail.ru>
2023-01-02 13:50:44 +0300

    Init

---
+++ links.py
@@ -0,0 +1,9 @@
+"""I want to make program to desktop that can open web links for my convenience"""
+
+from tkinter import *
+
+root = Tk ()
+root.geometry ('300x70+10+700')
+root.title ('links')
+
+root.mainloop ()
