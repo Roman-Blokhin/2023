dfd5fcce16b6b1bca8d29a0a2117ddb5846912b1
Roman <11-roman@mail.ru>
2023-01-02 15:19:45 +0300

    add buttons and columnconfigure

--- links.py
+++ links.py
@@ -6,4 +6,26 @@ root = Tk ()
 root.geometry ('300x70+10+700')
 root.title ('links')
 
+Button (text='GIT', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=0, column=0, stick='wens')
+
+Button (text='CodeWars', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=1, column=0, stick='wens')
+
+Button (text='Robo', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=0, column=1, stick='wens')
+
+Button (text='YouTube', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=1, column=1, stick='wens')
+
+Button (text='Color', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=0, column=2, stick='wens')
+
+Button (text='ICO', bd=3, bg='black', font=('Arial', 10, 'normal'), fg = 'white').\
+    grid(row=1, column=2, stick='we')
+
+root.grid_columnconfigure(0, minsize=80)
+root.grid_columnconfigure(1, minsize=80)
+root.grid_columnconfigure(2, minsize=80)
+
 root.mainloop ()
