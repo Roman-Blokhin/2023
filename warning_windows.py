a0c93439f9aba1bd0e4779e8936fe7f0c3360e04
Roman <11-roman@mail.ru>
2023-01-03 11:57:55 +0300

    add def for error button and different config

--- warning_windows.py
+++ warning_windows.py
@@ -11,6 +11,11 @@ def warning ():
     text = 'Warning! Don\'t make mistake!'
     mb.showwarning('Warning', text)
 
+def error ():
+    text = 'This is GAME OVER'
+    mb.showerror('Error', text)
+    root.config (bg = 'white')
+
 root = Tk ()
 root.geometry ('507x117+200+200')
 root.title ('Windows')
@@ -19,11 +24,11 @@ root.config (bg = 'grey')
 Button (text = 'Information', font = ('Arial', 15, 'italic'), bd = 5, bg = 'pink', fg = 'white', command=information).\
     grid (row= 0, column = 0, stick = 'wens', padx = 5, pady = 5)
 
-Button (text = 'Warning', font = ('Arial', 15, 'normal'), bd = 5, command=warning).\
-    grid (row= 0, column = 2, stick = 'wens', padx = 5, pady = 5)
+Button (text = 'Warning', font = ('Arial', 15, 'normal'), bd = 5, activeforeground='red', command=warning).\
+        grid (row= 0, column = 2, stick = 'wens', padx = 5, pady = 5)
 
-Button (text = 'Error', font = ('Arial', 15, 'bold'), bd = 5, bg = '#87CEEB').\
-    grid (row= 1, column = 1, stick = 'wens', padx = 5, pady = 5)
+Button (text = 'Error', font = ('Arial', 15, 'bold'), bd = 5, bg = '#87CEEB', activebackground='red',
+        command=error).grid (row= 1, column = 1, stick = 'wens', padx = 5, pady = 5)
 
 root.grid_columnconfigure (0, minsize=170)
 root.grid_columnconfigure (1, minsize=170)
