57e48a7da047a488526933a7ba0c3f7d273d7867
Roman <11-roman@mail.ru>
2023-01-03 09:20:38 +0300

    add buttons and there config

--- warning_windows.py
+++ warning_windows.py
@@ -3,7 +3,24 @@
 from tkinter import *
 
 root = Tk ()
-root.geometry ('390x50+200+200')
+root.geometry ('507x117+200+200')
 root.title ('Windows')
+root.config (bg = 'grey')
+
+Button (text = 'Information', font = ('Arial', 15, 'italic'), bd = 5, bg = 'pink', fg = 'white').\
+    grid (row= 0, column = 0, stick = 'wens', padx = 5, pady = 5)
+
+Button (text = 'Warning', font = ('Arial', 15, 'normal'), bd = 5).\
+    grid (row= 0, column = 2, stick = 'wens', padx = 5, pady = 5)
+
+Button (text = 'Error', font = ('Arial', 15, 'bold'), bd = 5, bg = '#87CEEB').\
+    grid (row= 1, column = 1, stick = 'wens', padx = 5, pady = 5)
+
+root.grid_columnconfigure (0, minsize=170)
+root.grid_columnconfigure (1, minsize=170)
+root.grid_columnconfigure (2, minsize=170)
+
+root.rowconfigure (1, minsize=30)
+root.rowconfigure (1, minsize=30)
 
 root.mainloop()
