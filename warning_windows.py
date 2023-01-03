177965542784b029707d4bf87fea9d915e49c71c
Roman <11-roman@mail.ru>
2023-01-03 11:33:53 +0300

    add def for warning button

--- warning_windows.py
+++ warning_windows.py
@@ -7,6 +7,10 @@ def information ():
     text = 'This is information window'
     mb.showinfo ('Info', text)
 
+def warning ():
+    text = 'Warning! Don\'t make mistake!'
+    mb.showwarning('Warning', text)
+
 root = Tk ()
 root.geometry ('507x117+200+200')
 root.title ('Windows')
@@ -15,7 +19,7 @@ root.config (bg = 'grey')
 Button (text = 'Information', font = ('Arial', 15, 'italic'), bd = 5, bg = 'pink', fg = 'white', command=information).\
     grid (row= 0, column = 0, stick = 'wens', padx = 5, pady = 5)
 
-Button (text = 'Warning', font = ('Arial', 15, 'normal'), bd = 5).\
+Button (text = 'Warning', font = ('Arial', 15, 'normal'), bd = 5, command=warning).\
     grid (row= 0, column = 2, stick = 'wens', padx = 5, pady = 5)
 
 Button (text = 'Error', font = ('Arial', 15, 'bold'), bd = 5, bg = '#87CEEB').\
