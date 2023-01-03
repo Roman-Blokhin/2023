ee4cb6848b468312d84329ee788f8527f42bc29c
Roman <11-roman@mail.ru>
2023-01-03 08:37:02 +0300

    init

---
+++ warning_windows.py
@@ -0,0 +1,9 @@
+"""I want to do some pop-up windows: information, warning and error"""
+
+from tkinter import *
+
+root = Tk ()
+root.geometry ('390x50+200+200')
+root.title ('Windows')
+
+root.mainloop()
