2d504fcaba69e79fe891e108a015d78861a79330
Roman <11-roman@mail.ru>
2023-01-05 08:00:21 +0300

    init

---
+++ gui_password_generation.py
@@ -0,0 +1,9 @@
+"""I want to do password generation with with tkinter"""
+
+from tkinter import *
+
+root = Tk ()
+root.geometry ('200x200+200+200')
+root.title ('Password Generation')
+
+root.mainloop()
