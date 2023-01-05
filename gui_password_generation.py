b54431493e28c52ecaba38e4bf5486d624c93d9c
Roman <11-roman@mail.ru>
2023-01-05 09:59:17 +0300

    add colors and new size for widgets and window

--- gui_password_generation.py
+++ gui_password_generation.py
@@ -13,19 +13,23 @@ def generate ():
     print (value)
 
 root = Tk ()
-root.geometry ('200x200+200+200')
+root.geometry ('300x100+200+200')
 root.title ('Password Generation')
+root.configure (bg = 'grey')
 
-Button(text='Generate', bd = 2, command=generate).\
-    grid(row=0, column=0, stick = 'wens', padx= 5, pady=5)
-
-name = Entry (root, width = 15, justify=RIGHT, bd = 2)
-name.grid (row=0, column=1, stick = 'wens', padx= 5, pady=5)
-
-Button (text = 'Clear', bd = 2, command = lambda: name.delete(0, END)).\
+Button(text='Generate', bd = 2, command=generate, bg = '#DCDCDC').\
     grid(row=1, column=0, stick = 'wens', padx= 5, pady=5)
 
-root.grid_columnconfigure(0, minsize=80)
-root.grid_columnconfigure(1, minsize=80)
+name = Entry (root, width = 15, justify=RIGHT, bd = 2, font = ('Arial', 11, 'normal'), bg = '#DCDCDC')
+name.grid (row=1, column=1, stick = 'wens', padx= 5, pady=5)
+
+Button (text = 'Clear', bd = 2, bg = '#DCDCDC', command = lambda: name.delete(0, END)).\
+    grid(row=2, column=0, stick = 'wens', padx= 5, pady=5)
+
+Label (text='You can generate password for you needs', bg = 'grey', fg = 'white', font = ('Arial', 11, 'normal')).\
+    grid (row=0,column=0, columnspan=2)
+
+root.grid_columnconfigure(0, minsize=150)
+root.grid_columnconfigure(1, minsize=150)
 
 root.mainloop()
