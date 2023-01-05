a6c72f10dca58c5fe822fff689240c16264d0766
Roman <11-roman@mail.ru>
2023-01-05 09:18:37 +0300

    import random, make generation password and lambda to our Buttons

--- gui_password_generation.py
+++ gui_password_generation.py
@@ -1,17 +1,24 @@
-"""I want to do password generation with with tkinter"""
+"""I want to do password generation with tkinter"""
 
 from tkinter import *
+import random as r
+
+chars = ('+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+         'абвгдеёжзийклмнопрстуфхцчшщэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ')
+password = r.sample(chars, 10)
+password = ''.join(password)
 
 root = Tk ()
 root.geometry ('200x200+200+200')
 root.title ('Password Generation')
 
-btn1 = Button(text='Generate', bd = 2).grid(row=0, column=0, stick = 'wens', padx= 5, pady=5)
+btn1 = Button(text='Generate', bd = 2, command=lambda: name.insert (0, password)).\
+    grid(row=0, column=0, stick = 'wens', padx= 5, pady=5)
 
-en = Entry (root, width = 15, justify=RIGHT, bd = 2)
-en.grid (row=0, column=1, stick = 'wens', padx= 5, pady=5)
+name = Entry (root, width = 15, justify=RIGHT, bd = 2)
+name.grid (row=0, column=1, stick = 'wens', padx= 5, pady=5)
 
-btn2 = Button (text = 'Clear', bd = 2, command = lambda: en.delete(0, END)).\
+btn2 = Button (text = 'Clear', bd = 2, command = lambda: name.delete(0, END)).\
     grid(row=1, column=0, stick = 'wens', padx= 5, pady=5)
 
 root.grid_columnconfigure(0, minsize=80)
