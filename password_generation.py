6a204edaf61931fffe9bbcf724e9bfed520f0c9a
Roman <11-roman@mail.ru>
2023-01-07 07:48:36 +0300

    made a non-fixed number of characters

--- password_generation.py
+++ password_generation.py
@@ -5,6 +5,8 @@ import random as r
 chars = ('+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
          'абвгдеёжзийклмнопрстуфхцчшщэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ') # указываем переменную с символами
 
-password = r.sample (chars, 10) # sample отвечает за перемешивание, далее число - сколько символов будет пароль
+i = int (input ('Выберите количество символов: '))
+
+password = r.sample (chars, i) # sample отвечает за перемешивание, далее число - сколько символов будет пароль
 password = ''.join(password) # прописываем, каким символом будут связаны наши элементы пароля
 print (password)
