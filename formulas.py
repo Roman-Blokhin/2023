4bf7d7c05ddf035507c85a99a7925e913424f1f3
Roman <11-roman@mail.ru>
2023-01-07 12:41:12 +0300

    init

---
+++ formulas.py
@@ -0,0 +1,10 @@
+"""I want to do program where i can use math formulas"""
+
+from tkinter import *
+
+root = Tk ()
+root.title ('Formulas')
+root.geometry ('350x350+200+200')
+root.config (bg = 'grey')
+
+root.mainloop ()
