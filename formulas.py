b5a5da88793cc90761024b638f36c066da4e0f7a
Roman <11-roman@mail.ru>
2023-01-07 15:14:12 +0300

    add label and grid_columnconfigure

--- formulas.py
+++ formulas.py
@@ -1,10 +1,20 @@
-"""I want to do program where i can use math formulas"""
+"""I want to do program where I can use math formulas"""
 
 from tkinter import *
 
 root = Tk ()
 root.title ('Formulas')
-root.geometry ('350x350+200+200')
+root.geometry ('400x400+200+200')
 root.config (bg = 'grey')
+
+Label (text = 'Помощник по математическим формулам', bg = 'grey', font = ('Arial', 12, 'normal'), fg = 'white')\
+    .grid(row = 0, column = 0, columnspan = 6, stick = 'wens')
+
+root.grid_columnconfigure(0, minsize = 50)
+root.grid_columnconfigure(1, minsize = 50)
+root.grid_columnconfigure(2, minsize = 50)
+root.grid_columnconfigure(3, minsize = 50)
+root.grid_columnconfigure(4, minsize = 50)
+root.grid_columnconfigure(5, minsize = 50)
 
 root.mainloop ()
