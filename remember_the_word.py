c3b9f0ce38eb08d85232838a90acbc9473ec8959
Roman <11-roman@mail.ru>
2023-01-08 09:13:06 +0300

    add sleep and lambda 'clear' for clear our win? but in pycharm's terminal don't work only console

--- remember_the_word.py
+++ remember_the_word.py
@@ -1,20 +1,27 @@
 """I want to do the game where you will remember the words"""
 
 import random as r
+from time import sleep
+import os
 
 while True:
     chars = ('+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
              'абвгдеёжзийклмнопрстуфхцчшщэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ')
 
-    conclusion = r.sample (chars, 3)
+    conclusion = r.sample(chars, 3)
     conclusion = ''.join(conclusion)
-    print ('\n', conclusion)
+    print('\n' + conclusion)
 
-    answer = input ('\nВведите слово: ')
+    sleep (2)
+
+    clear = lambda: os.system('cls')
+    clear()
+
+    answer = input('Введите слово: ')
 
     if answer == conclusion:
-        print ('\nУ вас отличная память!')
+        print('\nУ вас отличная память!')
     else:
-        print ('\nОй! Что-то пошло не так ;)')
+        print('\nОй! Что-то пошло не так ;)')
 
-    question = input ('\nДля продолжения нажмите Enter, для выхода 0: ')
+    question = input('\nДля продолжения нажмите Enter, для выхода 0: ')
