df554b58294e366f7b996de61bd7070212e9b484
Roman <11-roman@mail.ru>
2023-01-08 08:44:02 +0300

    init

---
+++ remember_the_word.py
@@ -0,0 +1,10 @@
+"""I want to do the game where you will be remember the words"""
+
+import random as r
+
+chars = ('+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+         'абвгдеёжзийклмнопрстуфхцчшщэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ')
+
+conclusion = r.sample (chars, 5)
+conclusion = ''.join(conclusion)
+print (conclusion)
