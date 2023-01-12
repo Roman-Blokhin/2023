64c3d2adc743b57caa59d1dafe975f65c6feaff9
Roman <11-roman@mail.ru>
2023-01-12 10:00:28 +0300

    add def 'time'. finish

--- gui_clock.py
+++ gui_clock.py
@@ -3,12 +3,23 @@
 from tkinter import *
 import time as t
 
+#функция отображения наших часов
+def time ():
+    clock = t.strftime ('%H:%M:%S %p')
+    l.config (text = clock)
+    l.after (1000, time) # устанавливаем параметры изменения нашего времени, 1000мс = 1с
+    print (clock)
+
 root = Tk ()
 root.title ('Clock')
-root.geometry ('200x80+200+200')
+root.geometry ('300x80+200+200')
 root.config (bg = 'black')
 
+# размещаем параметры текста на окне
 l = Label (root, font = ('Arial', 30), bg = 'black', fg = 'white')
 l.pack (anchor = 'center')
+
+# запускаем нашу функцию
+time()
 
 root.mainloop ()
