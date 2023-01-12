d0dd9a1dac80137dc33b088e4ab048f47a2c2e82
Roman <11-roman@mail.ru>
2023-01-12 09:42:39 +0300

    add label, import time

--- gui_clock.py
+++ gui_clock.py
@@ -1,10 +1,14 @@
 """I want to do clock with tkinter"""
 
 from tkinter import *
+import time as t
 
 root = Tk ()
 root.title ('Clock')
 root.geometry ('200x80+200+200')
 root.config (bg = 'black')
+
+l = Label (root, font = ('Arial', 30), bg = 'black', fg = 'white')
+l.pack (anchor = 'center')
 
 root.mainloop ()
