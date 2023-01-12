20ad9f815e77b95325e40fd4dcf2b5a3d1dd6661
Roman <11-roman@mail.ru>
2023-01-12 09:40:14 +0300

    init. clock - tkinter

---
+++ gui_clock.py
@@ -0,0 +1,10 @@
+"""I want to do clock with tkinter"""
+
+from tkinter import *
+
+root = Tk ()
+root.title ('Clock')
+root.geometry ('200x80+200+200')
+root.config (bg = 'black')
+
+root.mainloop ()
