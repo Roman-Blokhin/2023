72de862db15e52e5dfc738fe06877b83b6ee4eeb
Roman <11-roman@mail.ru>
2023-01-16 08:39:23 +0300

    add checkbuttons and change widget's size

--- gui_password_generation.py
+++ gui_password_generation.py
@@ -28,26 +28,37 @@ def generate ():
 
 
 
-# ------------------------------------------------------------- WIMDOW
+# ------------------------------------------------------------- WINDOW
 
 root = Tk ()
-root.geometry ('300x100+200+200')
+root.geometry ('350x230+200+200')
 root.title ('Password Generation')
 root.configure (bg = 'grey')
 
 # ------------------------------------------------------------- WIDJET
 
-Button(text='Generate', bd = 2, command=generate, bg = '#DCDCDC').\
-    grid(row=1, column=0, stick = 'wens', padx= 5, pady=5)
+Button(text='Generate', bd = 2, command=generate, bg = '#DCDCDC', font = ('Arial', 11, 'normal')).\
+    grid(row=4, column=0, stick = 'wens', padx= 5, pady=5)
 
 name = Entry (root, width = 15, justify=RIGHT, bd = 2, font = ('Arial', 11, 'normal'), bg = '#DCDCDC')
-name.grid (row=1, column=1, stick = 'wens', padx= 5, pady=5)
+name.grid (row=4, column=1, stick = 'wens', padx= 5, pady=5)
 
-Button (text = 'Clear', bd = 2, bg = '#DCDCDC', command = lambda: name.delete(0, END)).\
-    grid(row=2, column=0, stick = 'wens', padx= 5, pady=5)
+Button (text = 'Clear', bd = 2, bg = '#DCDCDC', font = ('Arial', 11, 'normal'), command = lambda: name.delete(0, END)).\
+    grid(row=5, column=0, stick = 'wens', padx= 5, pady=5)
 
-Label (text='You can generate password for you needs', bg = 'grey', fg = 'white', font = ('Arial', 11, 'normal')).\
+Label (text='You can generate password for you needs', bg = 'grey', fg = 'white', font = ('Arial', 12, 'bold')).\
     grid (row=0,column=0, columnspan=2)
+
+# ------------------------------------------------------------- Checkbuttons and variables
+
+one = Checkbutton (text = 'Letters', bg = 'grey', fg = 'black', font = ('Arial', 11, 'normal'))
+one.grid (row = 1, column = 0, stick = 'w', padx= 5, pady=5)
+
+two = Checkbutton (text = 'Symbols', bg = 'grey', fg = 'black', font = ('Arial', 11, 'normal'))
+two.grid (row = 2, column = 0, stick = 'w', padx= 5, pady=5)
+
+three = Checkbutton (text = 'Numbers', bg = 'grey', fg = 'black', font = ('Arial', 11, 'normal'))
+three.grid (row = 3, column = 0, stick = 'w', padx= 5, pady=5)
 
 # ------------------------------------------------------------- CONFIG
 
