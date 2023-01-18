abab3d610c12dbd542736ff26852c62fe1aa7ce0
Roman <11-roman@mail.ru>
2023-01-18 22:50:01 +0300

    add labels and configs

--- light_switch.py
+++ light_switch.py
@@ -7,14 +7,18 @@ root.title ('light switch')
 root.geometry ('525x500+200+200')
 root.config (bg = 'grey')
 
-Button (bd = 5).grid(row=0, column=1, sticky='wens', padx=5, pady=20)
-Button (bd = 5).grid(row=0, column=2, sticky='wens', padx=5, pady=20)
+Label (text=' ', bg = 'grey').grid(row=0, column=0)
+Label (text=' ', bg = 'grey').grid(row=1, column=3)
+Label (text=' ', bg = 'grey').grid(row=1, column=0)
 
-root.grid_columnconfigure(0, minsize=60)
+Button (bd = 5).grid(row=1, column=1, sticky='wens', padx=5, pady=20)
+Button (bd = 5).grid(row=1, column=2, sticky='wens', padx=5, pady=20)
+
+root.grid_columnconfigure(0, minsize=65)
 root.grid_columnconfigure(1, minsize=200)
 root.grid_columnconfigure(2, minsize=200)
 
-root.grid_rowconfigure(0, minsize=400)
-root.grid_rowconfigure(0, minsize=400)
+root.grid_rowconfigure(0, minsize=70)
+root.grid_rowconfigure(1, minsize=360)
 
 root.mainloop ()
