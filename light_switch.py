9023a071951f72abd3c647dd5593d88fb3999988
Roman <11-roman@mail.ru>
2023-01-18 22:59:38 +0300

    add def for light off

--- light_switch.py
+++ light_switch.py
@@ -2,16 +2,26 @@
 
 from tkinter import *
 
+def off ():
+    root.config(bg='black')
+    one.config (bg='black')
+    two.config (bg='black')
+    three.config (bg='black')
+
 root = Tk ()
 root.title ('light switch')
 root.geometry ('525x500+200+200')
 root.config (bg = 'grey')
 
-Label (text=' ', bg = 'grey').grid(row=0, column=0)
-Label (text=' ', bg = 'grey').grid(row=1, column=3)
-Label (text=' ', bg = 'grey').grid(row=1, column=0)
+one = Label (text=' ', bg = 'grey')
+one.grid(row=0, column=0)
+two = Label (text=' ', bg = 'grey')
+two.grid(row=1, column=3)
+three = Label (text=' ', bg = 'grey')
+three.grid(row=1, column=0)
 
-Button (bd = 5).grid(row=1, column=1, sticky='wens', padx=5, pady=20)
+Button (bd = 5, command= off).grid(row=1, column=1, sticky='wens', padx=5, pady=20)
+
 Button (bd = 5).grid(row=1, column=2, sticky='wens', padx=5, pady=20)
 
 root.grid_columnconfigure(0, minsize=65)
