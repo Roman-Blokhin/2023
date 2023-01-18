782cbebb526aef11bbe9ace2ab6108c04ba99ccb
Roman <11-roman@mail.ru>
2023-01-18 22:46:03 +0300

    add buttons and configs

--- light_switch.py
+++ light_switch.py
@@ -7,4 +7,14 @@ root.title ('light switch')
 root.geometry ('525x500+200+200')
 root.config (bg = 'grey')
 
+Button (bd = 5).grid(row=0, column=1, sticky='wens', padx=5, pady=20)
+Button (bd = 5).grid(row=0, column=2, sticky='wens', padx=5, pady=20)
+
+root.grid_columnconfigure(0, minsize=60)
+root.grid_columnconfigure(1, minsize=200)
+root.grid_columnconfigure(2, minsize=200)
+
+root.grid_rowconfigure(0, minsize=400)
+root.grid_rowconfigure(0, minsize=400)
+
 root.mainloop ()
