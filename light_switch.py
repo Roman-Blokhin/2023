31e234c4ac3a7f3d44897a8ab10039295e31f2ce
Roman <11-roman@mail.ru>
2023-01-18 22:45:13 +0300

    init

---
+++ light_switch.py
@@ -0,0 +1,10 @@
+"""I want to do light switch with tkinter"""
+
+from tkinter import *
+
+root = Tk ()
+root.title ('light switch')
+root.geometry ('525x500+200+200')
+root.config (bg = 'grey')
+
+root.mainloop ()
