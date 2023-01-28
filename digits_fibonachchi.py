fc280fb1c30cbef53fd979dc4783b28f66f052b0
Roman <11-roman@mail.ru>
2023-01-28 18:37:45 +0300

    and variable 'count'

--- digits_fibonachchi.py
+++ digits_fibonachchi.py
@@ -1,12 +1,15 @@
 # ПОСЛЕДОВАННОСТЬ ФИБОНАЧИ, КОГДА НОВОЕ ЧИСЛО, ЭТО СУММА ДВУХ ПРЕДЫДУЩИХ
 
 print ('ПЕРВЫЙ СПОСОБ:')
-fir, sec = 1, 1
+fir, sec, count = 1, 1, 0
 while sec < 1000:
+    count += 1
     print (sec)
     next_fir = fir + sec
     next_sec = fir
     fir, sec = next_fir, next_sec
+print ('Программой пройдено', count, 'чисел')
+
 
 # МОЖНО ЗАПИСАТЬ ДРУГИМ СПОСОБОМ
 print ('\nВТОРОЙ СПОСОБ:')
