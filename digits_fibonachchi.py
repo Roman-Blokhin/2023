fcc690010f93719a91fb28525d2743cae6d710f4
Roman <11-roman@mail.ru>
2023-01-28 17:53:11 +0300

    second way

--- digits_fibonachchi.py
+++ digits_fibonachchi.py
@@ -1,8 +1,17 @@
 # ПОСЛЕДОВАННОСТЬ ФИБОНАЧИ, КОГДА НОВОЕ ЧИСЛО, ЭТО СУММА ДВУХ ПРЕДЫДУЩИХ
 
+print ('ПЕРВЫЙ СПОСОБ:')
 fir, sec = 1, 1
 while sec < 1000:
     print (sec)
     next_fir = fir + sec
     next_sec = fir
     fir, sec = next_fir, next_sec
+
+# МОЖНО ЗАПИСАТЬ ДРУГИМ СПОСОБОМ
+print ('\nВТОРОЙ СПОСОБ:')
+
+a, b = 1, 1
+while b < 100:
+    print (b)
+    a, b = b, a + b
