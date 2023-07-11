a00db9a2992c1764ab8a139207de8dca6a4f6c64
Roman <11-roman@mail.ru>
2023-07-11 12:31:53 +0300

    добавил цикл и выход из программы

--- sport_engineer.py
+++ sport_engineer.py
@@ -1,51 +1,67 @@
 # ХОЧУ НАПИСАТЬ ПРОГРАММУ, КОТОРАЯ БУДЕТ ВЫВОДИТЬ СПИСОК УПРАЖНЕНИЙ ДЛЯ ОПРЕДЕЛЕННОЙ ГРУППЫ МЫШЦ В ОПРЕДЕЛЕННЫЙ ДЕНЬ
 
-choice_workout = "\nВыберите схему тренировок: "
-choice_day = "\nВыбрать день недели: "
+while True:
+    choice_workout = "\nВыберите схему тренировок: "
+    choice_day = "\nВыбрать день недели: "
 
-sh_1 = "\nСхема тренировок №1"
-sh_2 = "\nСхема тренировок №2"
+    sh_1 = "\nСхема тренировок №1"
+    sh_2 = "\nСхема тренировок №2"
 
-var_1 = "\n1. Вариант 1"
-var_2 = "\n2. Вариант 2"
+    var_1 = "\n1. Вариант 1"
+    var_2 = "\n2. Вариант 2"
 
-mo = "\n1. Понедельник"
-we = "\n2. Среда"
-fr = "\n3. Пятница"
+    mo = "\n1. Понедельник"
+    we = "\n2. Среда"
+    fr = "\n3. Пятница"
+    ex = "\n4. Выход"
 
-legs_shoulders = "\nНоги + Плечи"
-chest_biceps = "\nГрудь + Бицепс"
-back_triceps = "\nСпина + Трицепс"
+    legs_shoulders = "\nНоги + Плечи"
+    chest_biceps = "\nГрудь + Бицепс"
+    back_triceps = "\nСпина + Трицепс"
 
-print(mo, we, fr)
+    game_over = "\nДо свидания. Хорошей тренировки!"
 
-a = input(choice_day)
+    back = "\nДля продолжения нажмите: Enter, для выхода: 9 "
 
-if a == "1":
-    print(legs_shoulders)
-    print(var_1, var_2)
-    b = input(choice_workout)
-    if b == "1":
-        print(sh_1)
-    else:
-        print(sh_2)
+    print(mo, we, fr, ex)
 
-if a == "2":
-    print(chest_biceps)
-    print(var_1, var_2)
-    b = input(choice_workout)
-    if b == "1":
-        print(sh_1)
-    else:
-        print(sh_2)
+    choice_1_level = input(choice_day)
 
-if a == "3":
-    print(back_triceps)
-    print(var_1, var_2)
-    b = input(choice_workout)
-    if b == "1":
-        print(sh_1)
-    else:
-        print(sh_2)
+    if choice_1_level == "1":
+        print(legs_shoulders)
+        print(var_1, var_2)
+        choice_2_level = input(choice_workout)
+        if choice_2_level == "1":
+            print(sh_1)
+        else:
+            print(sh_2)
+
+    if choice_1_level == "2":
+        print(chest_biceps)
+        print(var_1, var_2)
+        choice_2_level = input(choice_workout)
+        if choice_2_level == "1":
+            print(sh_1)
+        else:
+            print(sh_2)
+
+    if choice_1_level == "3":
+        print(back_triceps)
+        print(var_1, var_2)
+        choice_2_level = input(choice_workout)
+        if choice_2_level == "1":
+            print(sh_1)
+        else:
+            print(sh_2)
+
+    if choice_1_level == "4":
+        print(game_over)
+        break
+
+    come_back = input (back)
+
+    if come_back == "9":
+        print(game_over)
+        break
 
 # возможность выбрать по отдельности часть тела для выдачи в результате упражнений
