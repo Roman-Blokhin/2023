28773ca32ec1dcf12d6c754b1bac52183eb98a10
Roman <11-roman@mail.ru>
2023-07-15 09:15:26 +0300

    добавил размер строк и колонок

--- sport_engineer_gui/main.py
+++ sport_engineer_gui/main.py
@@ -6,31 +6,39 @@ from tkinter import *
 
 root = Tk ()
 root.title ('Система тренировок')
-root.geometry ('700x700+50+50')
+root.geometry ('900x700+50+50')
 root.config (bg = 'grey')
 
 # ---------------------- 3. Создаем кнопки и лейблы -----------------------
 
 tk.Label(root, text=" ", bg="grey").grid(row=0, column=0)
 
-lbl_1 = tk.Label(root, text="    Выберите день тренировки:      ", bg="grey", font=("Arial", 12, "bold"), fg="white").\
-    grid(row=1, column=0, stick="we")
+lbl_1 = tk.Label(root, text="Выберите день тренировки:", bg="grey", font=("Arial", 16, "bold"), fg="white").\
+    grid(row=1, column=0, columnspan=3, stick="we")
 
 day_btn_mo = tk.Button(root, text="Понедельник", bg="grey", font=("Arial", 12, "bold"), fg="white").\
     grid(row=2, column=0, stick="w")
 day_btn_we = tk.Button(root, text="Среда", bg="grey", font=("Arial", 12, "bold"), fg="white").\
-    grid(row=3, column=0, stick="w")
+    grid(row=2, column=1, stick="w")
 day_btn_fr = tk.Button(root, text="Пятница", bg="grey", font=("Arial", 12, "bold"), fg="white").\
-    grid(row=4, column=0, stick="w")
+    grid(row=2, column=2, stick="w")
 
 # ---------------------- 4. Создаем окно для вывода информации -----------------------
 
-win = tk.Text(root, width=50, height=30, bg="white").grid(row=1, column=1, rowspan=16)
+win = tk.Text(root, width=50, height=30, bg="white")
+win.grid(row=1, column=3, rowspan=16)
 
+# ---------------------- 5. Присваиваем размер нашим строкам и столбцам -----------------------
 
+root.grid_columnconfigure(0, minsize=150)
+root.grid_columnconfigure(1, minsize=150)
+root.grid_columnconfigure(2, minsize=150)
+root.grid_columnconfigure(3, minsize=150)
 
-
-
+root.grid_rowconfigure(0, minsize=20)
+root.grid_rowconfigure(1, minsize=10)
+root.grid_rowconfigure(2, minsize=20)
+root.grid_rowconfigure(3, minsize=20)
 
 # ---------------------- 2. Закрываем программу -----------------------
 
