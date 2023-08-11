3af23281f32f5e05ac315dfe3cbb3cd47be2a5ab
Roman <11-roman@mail.ru>
2023-08-11 19:29:50 +0300

    создаю программку со всплывающими окнами

---
+++ virus/virus.py
@@ -0,0 +1,10 @@
+# создаю программку со всплывающими окнами
+
+import tkinter as tk
+from tkinter import messagebox as mb
+
+root = tk.Tk ()
+root.geometry ("500x500")
+root.title ("Безопасное приложение")
+
+root.mainloop ()
