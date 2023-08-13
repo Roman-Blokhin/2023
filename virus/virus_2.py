30614977259c319ac4f7e1d0e14cd4b0ce0910ab
Roman <11-roman@mail.ru>
2023-08-13 20:34:24 +0300

    при нажитии на кнопку запускается бесконечный цикл открытия сайтов

--- virus/virus_2.py
+++ virus/virus_2.py
@@ -1,5 +1,18 @@
-import tkinter as tk
 from tkinter import *
 from tkinter import messagebox as mb
 import random as r
 from random import randint
+import webbrowser as wb
+
+def open_git():
+    while True:
+        wb.open('https://github.com/')
+
+root = Tk ()
+root.title ('')
+root.geometry ('350x350+200+200')
+root.config (bg = '')
+
+Button (root, text="Не нажимать", command=open_git).pack()
+
+root.mainloop ()
