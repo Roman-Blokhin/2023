b4f60a1ed092f7130b135ccda4064c57a9ff5aba
Roman <11-roman@mail.ru>
2023-08-18 22:05:32 +0300

    создан цикл и отрисовка цвета на экране

--- new_pro/main.py
+++ new_pro/main.py
@@ -1,13 +1,40 @@
 import pygame as pg
 import random
 
+
+# 1 задаем переменные: размеры и цвета
 WIDTH = 400 # ширина экрана
 HEIGHT = 500 # высота экрана
 FPS = 30 # кол-во кадров в секунду
 
-# создаем игру и окно
+RED = 255, 0, 0
+GREEN = 0, 255, 0
+BLUE = 0, 0, 255
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+
+
+# 2 создаем игру и окно
 pg.init()
 pg.mixer.init() # для звука
-screen = pg.display.set_mode((WIDTH, HEIGHT)) # создаем экран
 pg.display.set_caption('WarCraft') # title
+screen = pg.display.set_mode((WIDTH, HEIGHT)) # создаем экран
 clock = pg.time.Clock() # игра работает с заданной частотой кадров
+
+# рендеринг - отрисовка
+screen.fill(RED) # экран красный для компьютера - обр. сторона доски - заполнять
+pg.display.flip() # поворачиваем обратную сторону доски к пользователю - сальто
+
+
+# 3 Цикл игры
+running = True
+a = 100000
+while running:
+    # Ввод процесса (события)
+    # Обновление
+    # Визуализация (сборка)
+    a -= 1
+    print(a)
+    if a == 0:
+        running = False
+
