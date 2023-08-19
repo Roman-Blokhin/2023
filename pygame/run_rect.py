03460cab50a33217458b1daad9399c66fd3d60dc
Roman <11-roman@mail.ru>
2023-08-19 23:02:37 +0300

    добавил движение предметам

--- pygame/run_rect.py
+++ pygame/run_rect.py
@@ -18,6 +18,10 @@ x = 10
 y = 10
 mouse_x = 200
 mouse_y = 400
+eye_l_x = 50
+eye_l_y = 50
+eye_r_x = 140
+eye_r_y = 50
 
 width = 20
 height = 20
@@ -27,7 +31,6 @@ velocity = 0.1
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-#pg.display.flip()
 pg.display.set_caption('WarCraft')
 
 
@@ -40,19 +43,32 @@ while run:
     keys = pg.key.get_pressed()
     if keys[pg.K_LEFT] and x > 0:
         mouse_x -= velocity
-    if keys[pg.K_RIGHT] and x < 400 - width:
+        eye_l_x -= velocity/4.5
+        eye_r_x -= velocity/4
+
+    elif keys[pg.K_RIGHT] and x < 300:
         mouse_x += velocity
-    if keys[pg.K_UP] and x > 0:
+        eye_l_x += velocity/4
+        eye_r_x += velocity/4.5
+
+    elif keys[pg.K_UP] and x > 0:
         mouse_y -= velocity
-    if keys[pg.K_DOWN] and x < 400 - height:
+        eye_l_y -= velocity/4.5
+        eye_r_y -= velocity/4
+
+    elif keys[pg.K_DOWN] and x < 300 :
         mouse_y += velocity
+        eye_l_y += velocity/4
+        eye_r_y += velocity/4
 
     screen.fill(GREEN) # пишем в цикле, чтобы объект не растягивался
 
     first = pg.draw.rect(screen, BLACK, (10, 10, WIDTH/2, HEIGHT/2))
-    second = pg.draw.rect(screen, WHITE, (50, 50, width, height))
-    third = pg.draw.rect(screen, WHITE, (140, 50, width, height))
+    second = pg.draw.rect(screen, WHITE, (eye_l_x, eye_l_y, width, height))
+    third = pg.draw.rect(screen, WHITE, (eye_r_x, eye_r_y, width, height))
     four = pg.draw.rect(screen, BLACK, (mouse_x, mouse_y, width, height))
     pg.display.update()
+
+
 pg.quit()
 
