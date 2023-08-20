78c560a9eb027957a2c25e44d12e20dd0bdd8a17
Roman <11-roman@mail.ru>
2023-08-20 21:57:01 +0300

    добавил яблоко для игры в змейку

--- pygame/rename.py
+++ pygame/rename.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 
 red = 100, 0, 0
-green = 0, 100, 0
+green = 0, 250, 0
 yellow = 250, 250, 0
 blue = 0, 0, 100
 
@@ -11,13 +12,17 @@ FPS = 30
 
 run = True
 
+hero_x = screen_width/2
+hero_y = screen_height/2
 hero_width = 20
 hero_height = 20
 
-hero_x = screen_width/2
-hero_y = screen_height/2
+velocity = 0.05
 
-velocity = 0.1
+apple_x = random.randint (5, 295)
+apple_y = random.randint (5, 395)
+apple_width = 15
+apple_height = 15
 
 pygame.init()
 win = pygame.display.set_mode((screen_width, screen_height))
@@ -27,6 +32,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    win.fill(red)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and hero_x > 5:
@@ -41,8 +48,10 @@ while run:
     if keys[pygame.K_DOWN] and hero_y + hero_height < screen_height - 4:
         hero_y += velocity
 
-    win.fill(red)
     pygame.draw.rect(win, yellow, (hero_x, hero_y, hero_width, hero_height))
+
+    pygame.draw.rect(win, green, (apple_x, apple_y, apple_width, apple_height))
+
     pygame.display.update()
 
 pygame.quit()
