a4e94b6f9f2bf80de0447d5e4272bbb0c01f7269
Roman <11-roman@mail.ru>
2023-08-21 10:41:24 +0300

    змейка движется автоматически

--- pygame/snake.py
+++ pygame/snake.py
@@ -8,7 +8,7 @@ blue = 0, 0, 100
 
 screen_width = 300
 screen_height = 400
-FPS = 30
+FPS = 7
 
 run = True
 
@@ -17,7 +17,8 @@ hero_y = screen_height/2
 hero_width = 20
 hero_height = 20
 
-velocity = 0.05
+x1_change = 0
+y1_change = 0
 
 apple_x = random.randint (5, 295)
 apple_y = random.randint (5, 395)
@@ -27,31 +28,36 @@ apple_height = 15
 pygame.init()
 win = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('WarCraft')
+clock = pygame.time.Clock()
 
 while run:
+    win.fill(red)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    win.fill(red)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x1_change = -10
+            y1_change = 0
+        if keys[pygame.K_RIGHT]:
+            x1_change = +10
+            y1_change = 0
+        if keys[pygame.K_UP]:
+            y1_change = -10
+            x1_change = 0
+        if keys[pygame.K_DOWN]:
+            y1_change = +10
+            x1_change = 0
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and hero_x > 5:
-        hero_x -= velocity
-
-    if keys[pygame.K_RIGHT] and hero_x + hero_width < screen_width - 4:
-        hero_x += velocity
-
-    if keys[pygame.K_UP] and hero_y > 5:
-        hero_y -= velocity
-
-    if keys[pygame.K_DOWN] and hero_y + hero_height < screen_height - 4:
-        hero_y += velocity
+    hero_x += x1_change
+    hero_y += y1_change
 
     pygame.draw.rect(win, yellow, (hero_x, hero_y, hero_width, hero_height))
-
     pygame.draw.rect(win, green, (apple_x, apple_y, apple_width, apple_height))
 
+    clock.tick(FPS)
     pygame.display.update()
 
 pygame.quit()
