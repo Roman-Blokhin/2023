9354607955426232bb324350b5cf4a6711365a76
Roman <11-roman@mail.ru>
2023-08-27 17:28:38 +0300

    добавили шрифты и вывели текст на экран

--- pygame/lesson_1.py
+++ pygame/lesson_1.py
@@ -1,15 +1,26 @@
 import pygame
+pygame.init() # для того чтобы текст выводился на экран
 
-SIZE = (300, 300)
+SIZE = (600, 500)
+TEXT_COLOR = (0, 155, 168)
+FRAME_COLOR = (224, 255, 255)
+YELLOW = (255, 255, 0)
+ORANGE_RED = 255, 69, 0
 
-pygame.display.set_mode(SIZE)
+screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Lesson one")
 img = pygame.image.load('icon.png') # загружаем картинку
 pygame.display.set_icon(img) # отображаем картинку
+
+font = pygame.font.SysFont('arial', 28) # параметры нашего шрифта
+text = font.render('Подключайся к программированию', 1, TEXT_COLOR, FRAME_COLOR) # текст с параметрами, 1 - сглаживание
+text_2 = font.render('Привет, Роман!', 0, YELLOW, ORANGE_RED)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
 
+    screen.blit(text, (110, 20)) # вызываем наш текст на экран + задаем координаты х и у
+    screen.blit(text_2, (0, 300))
     pygame.display.update()
