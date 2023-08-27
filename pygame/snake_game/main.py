7d30117a467d118f7390b4b132b5c7fb213eab65
Roman <11-roman@mail.ru>
2023-08-27 07:59:10 +0300

    добавил отступ сверху и расчертил границы игрового экрана

--- pygame/snake_game/main.py
+++ pygame/snake_game/main.py
@@ -1,15 +1,18 @@
 import pygame
 
-FRAME_COLOR = (0, 0, 0)
+FRAME_COLOR = (70, 130, 180)
 BLUE = (135, 206, 235)
 WHITE = (255, 255, 255)
-
-SIZE = [400, 400]
-SIZE_BLOCK = 20
-MARGIN = 1
+HEADER_COLOR = (173, 216, 230)
 
 COUNT_BLOCKS = 20
+SIZE_BLOCK = 20
+MARGIN = 1
+HEADER_MARGIN = 70
 
+SIZE = [SIZE_BLOCK*COUNT_BLOCKS + 2*SIZE_BLOCK + MARGIN*COUNT_BLOCKS,
+        SIZE_BLOCK*COUNT_BLOCKS + 2*SIZE_BLOCK + MARGIN*COUNT_BLOCKS + HEADER_MARGIN]
+print(SIZE)
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Snake')
@@ -20,16 +23,17 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
+    pygame.draw.rect(screen, HEADER_COLOR, [0, 0, SIZE[0], HEADER_MARGIN])
 
-    # создаем вложенный цикл, к котороый создает столбцы и колонки определенных цветов с отступами
+    # создаем вложенный цикл, который создает столбцы и колонки определенных цветов с отступами
     for row in range(COUNT_BLOCKS):
         for column in range(COUNT_BLOCKS):
             if (row + column) % 2 == 0:
                 color = BLUE
             else:
                 color = WHITE
-            pygame.draw.rect(screen, color, [0 + column * SIZE_BLOCK + MARGIN * (column + 1),
-                                             0 + row * SIZE_BLOCK + MARGIN * (row + 1),
+            pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
+                                             HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1),
                                              SIZE_BLOCK,
                                              SIZE_BLOCK])
 
