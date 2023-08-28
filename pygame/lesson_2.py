6d7e7a859bdd33b418d0c4b3a7e3d69520d9e4f1
Roman <11-roman@mail.ru>
2023-08-28 09:53:26 +0300

    создали первый ряд квадратов на игровом поле

--- pygame/lesson_2.py
+++ pygame/lesson_2.py
@@ -3,12 +3,15 @@
 import pygame
 pygame.init()
 
-size = (500,500)
+size = (510, 510)
+width = height = 40
+count_blocks = 10
+margin = 10
 
 FRAME_COLOR = (0, 0, 0)
 RED = (150, 0, 0)
 GREEN = (0, 150, 0)
-WHITE = (255, 255, 255)
+white = (255, 255, 255)
 
 win = pygame.display.set_mode(size)
 pygame.display.set_caption('Клеточное поле')
@@ -19,5 +22,10 @@ while True:
             print('Выход')
             quit()
     win.fill(FRAME_COLOR)
+
+    for column in range(count_blocks):
+        x = column*width + (column+1)*margin # меняем координату Х - колонку умножаем на ширину блока и суммируем с
+        # отступом умноженным на (колонку + 1) - так мы чередуем номер наших колонок
+        pygame.draw.rect(win, white, (x, 10, width, height))
 
     pygame.display.update()
