8e658a091050a1e7f1b6c843fd1e0ebbac1996d0
Roman <11-roman@mail.ru>
2023-08-30 22:49:40 +0300

    добавил условие изменение цвета для двух игроков

--- pygame/tic_tac_toe_game/tic_tac_toe_game.py
+++ pygame/tic_tac_toe_game/tic_tac_toe_game.py
@@ -1,8 +1,8 @@
 import pygame
 pygame.init()
 
-block_size = 100 # размер клеток
-margin = 15 # размер отступов между клетками
+block_size = 100
+margin = 15
 width = height = block_size*3 + margin*4 # ширина и длина окна
 size_window = (width, height)
 
@@ -10,6 +10,9 @@ FRAME_COLOR = (0, 0, 0)
 RED = (150, 0, 0)
 green = (0, 150, 0)
 white = (255, 255, 255)
+blue = (0, 0, 155)
+
+query = 0 # переменная для смены игрока
 
 win = pygame.display.set_mode(size_window)
 pygame.display.set_caption('Крестики-Нолики')
@@ -29,16 +32,22 @@ while True:
             print('x = ' + str(x_mouse) + ' y = ' + str(y_mouse))
             col = x_mouse // (block_size+margin)
             row = y_mouse // (block_size+margin)
-            mas [row][col] = 'x'
+            if query % 2 == 0:
+                mas [row][col] = 'x'
+            else:
+                mas [row][col] = 'o'
+            query += 1
 
     for row in range (3):
         for col in range (3):
             if mas [row][col] == 'x':
                 color = green
+            elif mas [row][col] == 'o':
+                color = blue
             else:
-                mas [row][col] = 'o'
                 color = white
             x = col*block_size + (col+1)*margin
             y = row*block_size + (row+1)*margin
-            pygame.draw.rect(win, RED, (x, y, block_size, block_size))
+            pygame.draw.rect(win, color, (x, y, block_size, block_size))
+
     pygame.display.update()
