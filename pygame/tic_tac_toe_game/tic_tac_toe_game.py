fa8f9ae879e7e9bfe19cf9d92921ef3b4ab63a22
Roman <11-roman@mail.ru>
2023-08-30 20:00:49 +0300

    продолжаю готовить поле для игры крестики-нолики

--- pygame/tic_tac_toe_game/tic_tac_toe_game.py
+++ pygame/tic_tac_toe_game/tic_tac_toe_game.py
@@ -8,21 +8,37 @@ size_window = (width, height)
 
 FRAME_COLOR = (0, 0, 0)
 RED = (150, 0, 0)
-GREEN = (0, 150, 0)
-WHITE = (255, 255, 255)
-
-mas = [[0]*3 for i in range (3)] # массив с нулями. нули означают, что клетка пустая
+green = (0, 150, 0)
+white = (255, 255, 255)
 
 win = pygame.display.set_mode(size_window)
 pygame.display.set_caption('Крестики-Нолики')
 img = pygame.image.load('tic_tac_icon.png')
 pygame.display.set_icon(img)
 
+mas = [[0]*3 for i in range (3)] # массив с нулями. нули означают, что клетка пустая
+
 while True:
+    win.fill(FRAME_COLOR)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print('Выход')
             quit()
-    win.fill(FRAME_COLOR)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            print('x = ' + str(x_mouse) + ' y = ' + str(y_mouse))
+            col = x_mouse // (block_size+margin)
+            row = y_mouse // (block_size+margin)
+            mas [row][col] = 'x'
 
+    for row in range (3):
+        for col in range (3):
+            if mas [row][col] == 'x':
+                color = green
+            else:
+                mas [row][col] = 'o'
+                color = white
+            x = col*block_size + (col+1)*margin
+            y = row*block_size + (row+1)*margin
+            pygame.draw.rect(win, RED, (x, y, block_size, block_size))
     pygame.display.update()
