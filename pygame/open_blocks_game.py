2fd1ada7721bbe7595a7ed558ed9e052e1bb6a34
Roman <11-roman@mail.ru>
2023-09-02 20:58:21 +0300

    вывод в консоль очередность игрока + отображение крестика и нолика на ячейках после хода

--- pygame/open_blocks_game.py
+++ pygame/open_blocks_game.py
@@ -29,26 +29,36 @@ while run:
             quit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x_mouse, y_mouse = pygame.mouse.get_pos()
-            print('x = ' + str(x_mouse) + ' y = ' + str(y_mouse))
             column = x_mouse // (block_size+margin)
             row = y_mouse // (block_size+margin)
             if mas [row] [column] == 0:
                 if query % 2 == 0:
                     mas [row] [column] = 'x'
+                    print('Ходит игрок: Х')
+                    print('x = ' + str(x_mouse) + ' y = ' + str(y_mouse))
                 else:
                     mas [row] [column] = 'o'
+                    print('Ходит игрок: О')
+                    print('x = ' + str(x_mouse) + ' y = ' + str(y_mouse))
             query += 1
 
     for row in range(block_count):
         for column in range(block_count):
             if mas [row] [column] == 'x':
                 color = green
+
             elif mas [row] [column] == 'o':
                 color = blue
+
             else:
                 color = grey
             x = column*block_size + (column+1)*margin
             y = row*block_size + (row+1)*margin
             pygame.draw.rect(screen, color, (x, y, block_size, block_size))
+            if color == green:
+                pygame.draw.line(screen, white, (x+20, y+20), (x + block_size-20, y + block_size-20), 3)
+                pygame.draw.line(screen, white, (x+block_size-20, y+20), (x+20, y + block_size-20), 3)
+            elif color == blue:
+                pygame.draw.circle(screen, white, (x+block_size/2, y+block_size/2), 35, 3)
 
     pygame.display.update()
