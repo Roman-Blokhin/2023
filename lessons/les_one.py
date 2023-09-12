423edebd3713ec75a3b4a7091e55ce2d7a870024
Roman <11-roman@mail.ru>
2023-09-12 21:41:56 +0300

    изучаю ООП и классы, методы в классе

--- lessons/les_one.py
+++ lessons/les_one.py
@@ -5,16 +5,25 @@ class Dog: # создали класс
     age = None
     isHappy = None
 
-dog_1 = Dog() # создаем объект на основе класса
-dog_1.name = 'Robbie'
-dog_1.age = 1.5
-dog_1.isHappy = True
+    def set_data(self, name, age, isHappy): # метод, он поможет сократить определение данных в новых объектах
+        self.name = name
+        self.age = age
+        self.isHappy = isHappy
 
-dog_2 = Dog()
+    def get_data(self): # создали метод, который выводит все параметры объекта
+        print('Имя:', self.name, 'Возраст:', self.age, 'Счастье:', self.isHappy )
+
+dog_1 = Dog() # создаем объект на основе класса
+dog_1.set_data('Robbie', 1.5, True) # используем метод в нашем классе
+
+dog_2 = Dog() # используем поля/переменные в нашем классе
 dog_2.name = 'William'
 dog_2.age = 4
 dog_2.isHappy = False
 
-print(dog_1.name)
+print(dog_1.isHappy) # выводим частичную информацию через поле(переменную)
 print(dog_2.name)
+
+dog_1.get_data() # выводим всю информацию через метод
+dog_2.get_data()
 
