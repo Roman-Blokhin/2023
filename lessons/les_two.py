81f21fc9ba0a260a0cbf936687e315d435dcf884
Roman <11-roman@mail.ru>
2023-09-14 09:24:18 +0300

    изучаю новые встроенные функции python + создал файл горячих клавиш

---
+++ lessons/les_two.py
@@ -0,0 +1,11 @@
+name = 'Roman'
+print(name.split()) # создает и добавляет строку в список
+
+list_1 = ['a', 'm', 't']
+print('.'.join(list_1)) # объединяет список в строку, используя указанный символ
+
+text = 'dgndhmgm gfnfgnf fgjgkhjhj Roman Roman'
+lookingForText = 'Roman'
+print(text.count(lookingForText)) # в строке ищет заданный текст
+
+print(name.isspace()) # является ли строка пробелом
