c59d9281370d5021207065bb92589ee66a7513e6
Roman <11-roman@mail.ru>
2023-09-17 21:45:59 +0300

    нарисовал кубики на экране

--- pygame/stones.py
+++ pygame/stones.py
@@ -9,6 +9,7 @@ blue = (0, 0, 150)
 
 block_size = 100
 margin = 50
+block_count = 2
 width = height = block_size*2 + margin*3
 size = (width, height)
 
@@ -17,6 +18,21 @@ secondNumber = ra.randint(1, 6)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('stones..')
+
+run = True
+while run:
+    screen.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    for row in range(block_count):
+        for column in range(1):
+            x = column*block_size + (column+1) * margin/2
+            y = row*block_size + (row+1) * margin
+            pygame.draw.rect(screen, black, (x, y, block_size, block_size))
+
+    pygame.display.update()
 
 print('Первый кубик: ', str (firstNumber))
 print('Второй кубик: ', str (secondNumber))
