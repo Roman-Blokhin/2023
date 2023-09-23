2a220c7483a9e193f9ef68779196059fe5d748db
Roman <11-roman@mail.ru>
2023-09-23 22:03:37 +0300

    задание: изменить количество столбцов

--- task_array.py
+++ task_array.py
@@ -15,45 +15,46 @@ print()
 import random as r
 
 n = 5
-mas = [[0 for i in range(n)] for j in range(n)] # создаем массив, колонки и столбцы, присваивая объектам значение 0
+m = 7
+mas = [[0 for i in range(m)] for j in range(n)] # создаем массив, столбцы и строки, присваивая объектам значение 0
 
 for i in range(n): # создаем условие, которое присваивает всем объектам в массиве рандомное число
-    for j in range(n):
+    for j in range(m):
         mas[i][j] = r.randint(0, 9)
 
 for i in range(n): # перебираем все объекты в строчке для вывода
-    for j in range(n): # перебираем все объекты в колонке для вывода
+    for j in range(m): # перебираем все объекты в колонке для вывода
         print(mas[i][j], end=' ')
     print()
 
 for i in range(n): # создаем условие, которое присваивает значение 20 всем объектам на главной диагонали
-    for j in range(n):
+    for j in range(m):
         if i == j:
             mas[i][j] = 20
 print()
 
 for i in range(n): # выводим уже новую матрицу с обновленными значениями
-    for j in range(n):
+    for j in range(m):
         print(mas[i][j], end=' ')
     print()
 
 # получаем сумму чисел в массиве
 s = 0
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         s += mas[i][j] # складываем все числа в массиве
 print (s)
 
 # заменяем числа, которые делятся на число 7 без остатка на значок амперсанда
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         if mas[i][j] % 7 == 0:
             mas[i][j] = "&"
 
 print ()
 
 for i in range(n): # выводим уже новую матрицу с обновленными значениями амперсанда
-    for j in range(n):
+    for j in range(m):
         print(mas[i][j], end=' ')
     print()
 
