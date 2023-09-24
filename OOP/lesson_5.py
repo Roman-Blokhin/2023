88a43391d2669659bb279ef616b6eae2260f2267
Roman <11-roman@mail.ru>
2023-09-24 07:43:02 +0300

    Методы экземпляра. Аргумент self

--- OOP/lesson_5.py
+++ OOP/lesson_5.py
@@ -2,8 +2,15 @@
 
 
 class Cat:
-    def hello (self):
-        print ('Hello, Roman')
+    breed = 'pers'
 
-a = Cat()
-print(Cat.hello(a))
+    def hello(*args):  # принимаем произвольное кол-во аргументов
+        print('Hello, Roman', args)
+
+    def show_breed(instance):  # прописываем объект, к которому обращаемся
+        print(f'Hello Roman = {instance.breed}')  # обращаемся к объекту через Ф строку
+
+
+bob = Cat()  # добавляем в класс новый экземпляр
+
+bob.show_breed()  # вызываем метод класса
