d9d4c21446d5bcc5fe7420ddf6e0ac3bdebdf717
Roman <11-roman@mail.ru>
2023-09-26 22:18:28 +0300

    оптимизация. используем уже созданный метод для упрощения кода

--- OOP/lesson_7.py
+++ OOP/lesson_7.py
@@ -2,9 +2,11 @@
 
 class Point:
     def __init__(self, coord_x=0, coord_y=0):  # коорд передаем по умолчанию, чтобы не было ошибки при их отсутствии
-        self.x = coord_x
-        self.y = coord_y
+        self.move_to(coord_x, coord_y)
 
     def move_to(self, new_x, new_y):
         self.x = new_x
         self.y = new_y
+
+    def go_home(self):
+        self.move_to(0, 0)  # используем уже созданный метод для упрощения кода
