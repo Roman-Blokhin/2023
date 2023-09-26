56fb0c6859ac681dacb56f31dd761bee9d357a59
Roman <11-roman@mail.ru>
2023-09-26 21:57:20 +0300

    создали метод перемещения точки

--- OOP/lesson_7.py
+++ OOP/lesson_7.py
@@ -2,6 +2,9 @@
 
 class Point:
     def __init__(self, coord_x=0, coord_y=0):  # коорд передаем по умолчанию, чтобы не было ошибки при их отсутствии
-        self.coord_x = coord_x
-        self.coord_y = coord_y
+        self.x = coord_x
+        self.y = coord_y
 
+    def move_to(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
