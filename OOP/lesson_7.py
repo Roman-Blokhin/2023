367fd732ea3e47e33d8b8a26e5c67585ae195e8e
Roman <11-roman@mail.ru>
2023-09-27 22:28:36 +0300

    создали список для добавления новых экземпляров, можем вывести координаты экземпляров по индексу

--- OOP/lesson_7.py
+++ OOP/lesson_7.py
@@ -3,8 +3,12 @@
 from math import sqrt  # импортируем функцию корня из библиотеки math
 
 class Point:
+
+    list_point = []  # создали список в который будем добавлять все новые точки в виде объектов с координатами
+
     def __init__(self, coord_x=0, coord_y=0):  # коорд передаем по умолчанию, чтобы не было ошибки при их отсутствии
         self.move_to(coord_x, coord_y)
+        Point.list_point.append(self)  # внутри класса вызываем экземпляр через имя самого класса
 
     def move_to(self, new_x, new_y):
         self.x = new_x
@@ -23,8 +27,16 @@ class Point:
 
         return sqrt((self.x - another_point.x)**2 + (self.y - another_point.y)**2)
 
+
 a = Point(6, 0)
 b = Point(0, 8)
 
 print (a.distance(b))
+
+print(Point.list_point)
+print(Point.list_point[0].x)  # по индексу мы обращаемся к объекту в списке и можем вывести координаты x и у
+print(Point.list_point[0].y)
+print(Point.list_point[1].x)
+print(Point.list_point[1].y)
+
 
