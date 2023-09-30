d500179b5ecfa4e78f0b794caa02533053c56c6c
Roman <11-roman@mail.ru>
2023-09-30 20:33:26 +0300

    работа с защищенными (protected) данными

--- OOP/lesson_9.py
+++ OOP/lesson_9.py
@@ -3,12 +3,20 @@
 class BankAccount:
 
     def __init__(self, name, balance, passport):
-        self.name = name
-        self.balance = balance
-        self.passport = passport
+        # self.name = name  # публичные данные
+        # self.balance = balance # публичные данные
+        # self.passport = passport # публичные данные
+        self._name = name  # защищенные данные
+        self._balance = balance # защищенные данные
+        self._passport = passport # защищенные данные
 
-    def print_public_data(self): # создали публичную функцию, которая выводит данные как внутри банка, так и вне банка
-        print(self.name, self.balance, self.passport)
+    # def print_public_data(self): # создали публичную функцию, которая выводит данные как внутри банка, так и вне банка
+    #     print(self.name, self.balance, self.passport)
+
+    # на  уровне согласования между разработчиками временно ставится protected - данные с нижним подчеркиванием впереди
+    def print_protected_data(self): # создали публичную функцию, которая выводит данные как внутри банка,
+        # так и вне банка
+        print(self._name, self._balance, self._passport)
 
 account_1 = BankAccount('Roman', 1000, 5670987)
-account_1.print_public_data()
+account_1.print_protected_data()
