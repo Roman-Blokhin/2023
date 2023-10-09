c340b524b7c381485e10fc16e65bf371d0451e49
Roman <11-roman@mail.ru>
2023-10-09 21:20:02 +0300

    вспоминаем, какие есть переменные - локальные, объемлющие, глобальные

--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -1,13 +1,21 @@
 # Вложенные функции Python
 
+g = 'gray'  # глобальная переменна
+
+
 def colors():
+    y = 'yellow'  # объемлющая переменная
+    g = 'green'
+
     def print_red():
-        r = 'red'
-        print(r)
+        nonlocal y  # позволяет менять объемлющую переменную
+        r = 'red'  # локальная переменная в функции
+        print(r, y, g)
+        y = 'changed'
 
     def print_blue():
         b = 'blue'
-        print(b)
+        print(b, y, g)
 
     print_red()
     print_blue()
