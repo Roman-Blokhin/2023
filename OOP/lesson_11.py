6d2a0915a63fd6a149af96660e6599981c02e6af
Roman <11-roman@mail.ru>
2023-10-20 21:30:55 +0300

    задекорировали функции геттер, сеттер и делитер, чтобы их было проще вызывать

--- OOP/lesson_11.py
+++ OOP/lesson_11.py
@@ -6,25 +6,28 @@ class BankAccount:
         self.name = name
         self.__balance = balance
 
-    def get_balance(self):
+    @property  # 1. создаем декоратор свойства геттер
+    def my_balance(self):  # 2. меняем название функции на my_balance, потому что функция задекорирована
         print('get balance')
         return self.__balance
 
-    def set_balance(self, value):
+    @my_balance.setter  # 3. декорируем сеттер, но вызываем его как свойство, чтобы не было совпадения имен
+    def my_balance(self, value):  # 4. меняем название, потому что задекорирована функция
         if not isinstance(value, (int, float)):
             raise ValueError ('Баланс должен быть числом')
         print('set balance')
         self.__balance = value
 
-    def delete_balance(self):
+    @my_balance.deleter  # 5. декорируем делитер
+    def my_balance(self):  # 6. меняем название, потому что задекорирована функция
         print('delete balance')
         del self.__balance
 
     # меняем запись свойства, делаем удобней
-    my_balance = property(get_balance)
-    # my_balance = my_balance.getter(get_balance)  # геттер может принимать сразу наше свойство  my_balance
-    my_balance = my_balance.setter(set_balance)
-    my_balance = my_balance.deleter(delete_balance)
+#    my_balance = property(get_balance)
+#    # my_balance = my_balance.getter(get_balance)  # геттер может принимать сразу наше свойство  my_balance
+#    my_balance = my_balance.setter(set_balance)
+#    my_balance = my_balance.deleter(delete_balance)
 
 
 
