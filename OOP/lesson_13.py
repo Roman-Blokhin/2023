0956f943e1583f08d73fe2a1036eada51c92be7d
Roman <11-roman@mail.ru>
2023-10-25 17:40:08 +0300

    сделали проверку - содержится ли в новом пароле хотя бы одна цифра

--- OOP/lesson_13.py
+++ OOP/lesson_13.py
@@ -1,29 +1,41 @@
 # ПРАКТИКА С property
 
+from string import digits  # импортируем digits - все числа 0-9, записанные строкой
+
+
 class User:
     def __init__(self, login, password):
         self.__login = login
         self.__password = password
 
     @property
-    def password (self):
+    def password(self):
         return self.__password
 
+    @staticmethod  # чтобы self не применялся, вешаем декоратор (не вызывает экземпляр класса)
+    def is_include_number(password):
+        for i in digits:  # пробегаемся по нашему паролю, если там есть цифра - True
+            if i in password:
+                return True
+
+        return False  # если в цикле цифры нет, возвращаем False
+
     @password.setter
-    def password (self, value):
-        if not isinstance(value, str):
-            raise TypeError ('Пароль должен быть текстом')
-        if len(value) < 4:
-            raise ValueError ('Длина пароль не может быть меньше 4 символов')
-        if len(value) > 10:
-            raise ValueError ('Длина пароль не может быть больше 10 символов')
+    def password(self, value):
+        if not isinstance(value, str):  # проверяет наш пароль, является ли он строкой
+            raise TypeError('Пароль должен быть текстом')
+        if len(value) < 4:  # проверяет пароль на минимальное количество символов
+            raise ValueError('Длина пароль не может быть меньше 4 символов')
+        if len(value) > 10:  # проверяет пароль на максимальное количество символов
+            raise ValueError('Длина пароль не может быть больше 10 символов')
+        if not User.is_include_number(value):  # проверка, содержится ли в новом пароле хотя бы одна цифра
+            raise ValueError('Пароль должен содержать хоть одну цифру')
         self.__password = value
 
     @property
-    def login (self):
+    def login(self):
         return self.__login
 
     @login.setter
-    def login (self, value):
+    def login(self, value):
         self.__login = value
-
