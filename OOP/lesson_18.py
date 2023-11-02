359c2f81b3445278aa14e911537522c0580a87c8
Roman <11-roman@mail.ru>
2023-11-02 21:58:47 +0300

    можем складывать экземпляр с новым экземпляром

--- OOP/lesson_18.py
+++ OOP/lesson_18.py
@@ -6,7 +6,17 @@ class BankAccount:
         self.name = name
         self.balance = balance
 
-    def __add__(self, other):  # сложение, нужен второй аргумент, первый - наш клиент, второй - новый баланс
+    def __add__(self, other):  # 1. сложение, нужен второй аргумент, первый - наш клиент, второй - новый баланс
         print('__add__ был вызван')
-        if isinstance(other, (int, float)):  # если значение принадлежит к типам int или float
+        if isinstance(other, BankAccount):  # 3. проверка, если второй аргумент - экземпляр класса, то складываем
+            return self.balance + other.balance  # 4. берем у экземпляра класса значение баланса
+        if isinstance(other, (int, float)):  # 2. если значение принадлежит к типам int или float
             return self.balance + other
+        raise NotImplemented  # 5. говорим, что для остальных типов операция не поддерживается
+
+
+
+q = BankAccount('oi', 100)
+w = BankAccount('oi', 1)
+print(q+w)  # можно менять местами элементы при сложении, если это экземпляры класса, а не экземпляр + новое знач.
+print(w+q)
