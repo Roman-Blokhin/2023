cdefd52b8b585a1765ad6989247a8e9283a17945
Roman <11-roman@mail.ru>
2023-11-03 22:10:58 +0300

    проверяем то, какой экземпляр меньше по площади

--- OOP/lesson_19.py
+++ OOP/lesson_19.py
@@ -12,7 +12,16 @@ class Rect:
         self.a = a
         self.b = b
 
+    @property  # 3. находим площадь прямоугольника, чтобы сравнить экземпляры знаком <
+    def area(self):
+        return self.a * self.b
+
     def __eq__(self, other):
         print('__eq__ вызван')
-        if isinstance(other, Rect):  # если поступающие данные равны нашему экземпляру класса
-            return self.a == other.a and self.b == other.b  # выводит True, если экземпляры равны
+        if isinstance(other, Rect):  # 1. если поступающие данные равны нашему экземпляру класса
+            return self.a == other.a and self.b == other.b  # 2. выводит True, если экземпляры равны
+
+    def __lt__(self, other):  # 4. проверяем знаком меньше: <
+        print('__lt__ вызван')
+        if isinstance(other, Rect):
+            return self.area < other.area
