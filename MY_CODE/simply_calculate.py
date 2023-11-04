46509cc769e05066f2fad2dc7d099961399ff181
Roman <11-roman@mail.ru>
2023-11-04 10:06:53 +0300

    добавил цвета и исключения

--- MY_CODE/simply_calculate.py
+++ MY_CODE/simply_calculate.py
@@ -1,7 +1,26 @@
-while True:
+from colorama import Fore, Back, Style
 
-    a = int(input('\nВведите первое число: '))
-    b = int(input('Введите второе число: '))
+while True:
+    try:
+        a = int(input('\nВведите первое число: '))
+    except ValueError:
+        print(Fore.BLUE + '\nВы не ввели число' + Style.RESET_ALL)
+        try:
+            a = int(input('\nВведите первое число: '))
+        except ValueError:
+            print(Fore.BLUE + '\nНу, как знаете.. До свидания.' + Style.RESET_ALL)
+            break
+
+    try:
+        b = int(input('Введите второе число: '))
+    except ValueError:
+        print(Fore.BLUE + '\nВы не ввели число' + Style.RESET_ALL)
+        try:
+            b = int(input('\nВведите второе число: '))
+        except ValueError:
+            print(Fore.BLUE + '\nНу, как знаете.. До свидания.' + Style.RESET_ALL)
+            break
+
     print('\n1. + \n2. - \n3. * \n4. / \n5. Exit')
     c = input('\nВыберите действие: ')
 
@@ -11,22 +30,24 @@ while True:
     div = a / b
 
     if c == '1':
-        print(f'Результат: {summ}')
+        print(f'Результат: ' + Fore.YELLOW + f'{summ}' + Style.RESET_ALL)
 
     if c == '2':
-        print(f'Результат: {sub}')
+        print(f'Результат: ' + Fore.YELLOW + f'{sub}' + Style.RESET_ALL)
 
     if c == '3':
-        print(f'Результат: {mul}')
+        print(f'Результат: ' + Fore.YELLOW + f'{mul}' + Style.RESET_ALL)
 
     if c == '4':
-        print(f'Результат: {div}')
+        print(f'Результат: ' + Fore.YELLOW + f'{div}' + Style.RESET_ALL)
 
     if c == '5':
-        print('\nGAME OVER')
+        print(Fore.RED + '\nДо свидания' + Style.RESET_ALL)
         break
 
-    enter = input('\nНажмите Enter, чтобы продолжить, и 1 для выхода: ')
+    enter = input('\nНажмите ' + Fore.BLUE + 'Enter' + Style.RESET_ALL + ', чтобы продолжить, или '
+                  + Fore.BLUE + '1' + Style.RESET_ALL + ' для выхода: ' + Style.RESET_ALL)
 
     if enter == '1':
+        print(Fore.RED + '\nДо свидания' + Style.RESET_ALL)
         break
