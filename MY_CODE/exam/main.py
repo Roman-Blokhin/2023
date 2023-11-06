41bf8237c2fc25f97a26be95f57c6d891ca91248
Roman <11-roman@mail.ru>
2023-11-06 19:43:29 +0300

    написал первый слой приложения без распределения по размерам, просто остов

--- MY_CODE/exam/main.py
+++ MY_CODE/exam/main.py
@@ -1,8 +1,35 @@
 from tkinter import *
 
-root = Tk ()
-root.title ('')
-root.geometry ('350x350+200+200')
-root.config (bg = '')
+root = Tk()
+root.title('')
+root.geometry('350x350+200+200')
+root.config(bg='')
 
-root.mainloop ()
+# --------------------------- #
+
+lbl1 = Label(root, text='fdgdfg', bg='grey')
+lbl1.grid(row=0, column=1)
+
+lbl1 = Label(root, text='fdgdfg', bg='grey')
+lbl1.grid(row=0, column=0)
+
+lbl1 = Label(root, text='fdgdfg', bg='grey')
+lbl1.grid(row=0, column=2)
+
+# --------------------------- #
+
+btn1 = Button(root, text='fdgdfg')
+btn1.grid(row=1, column=1)
+
+btn1 = Button(root, text='fdgdfg')
+btn1.grid(row=2, column=1)
+
+btn1 = Button(root, text='fdgdfg')
+btn1.grid(row=3, column=1)
+
+# --------------------------- #
+
+win = Text(root, bg='pink')
+win.grid(row=1, column=3)
+
+root.mainloop()
