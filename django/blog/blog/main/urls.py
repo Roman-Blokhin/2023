b32b62804872c6fd7e50f271664b98c826c6734a
Roman <11-roman@mail.ru>
2023-11-08 22:01:33 +0300

    написали методы для перехода на разные страницы ,если нужно, прописываем в файле views.py дополнительные паттерны

--- django/blog/blog/main/urls.py
+++ django/blog/blog/main/urls.py
@@ -3,4 +3,5 @@ from . import views  # 1. подключаем файл views
 
 urlpatterns = [
     path('', views.index),  # 2. добавляем ссылку на метод из файла views
+    path('about', views.about),
 ]
--- django/blog/blog/main/views.py
+++ django/blog/blog/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse  # 1. произвели импорт
 
-# Create your views here.
+def index(request):  # 2. создали первый метод
+    return HttpResponse('<h1>Проверка работы</h1>')  # 3. прописываем, что возвращаем
 
-def index():  # создали первый метод
-    pass
+def about(request):
+    return HttpResponse('<h3 style="color: ff0000"><i>Информация о компании</i></h3>')
