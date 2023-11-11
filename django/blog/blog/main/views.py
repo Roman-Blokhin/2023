39626a8739d95d472239493908ff57327ff0c751
Roman <11-roman@mail.ru>
2023-11-11 21:59:23 +0300

    функция render, создали новые папки для шаблонов html страниц. подключили шаблон главной страницы

--- django/blog/blog/main/views.py
+++ django/blog/blog/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # 1. произвели импорт
 
-def index(request):  # 2. создали первый метод
-    return HttpResponse('<h1>Проверка работы</h1>')  # 3. прописываем, что возвращаем
+# HttpResponse не может корректно выгрузить нам всю страницу, поэтому лучше использовать render
+def index(request): # загружаем шаблон из папки templates
+    return render(request, 'main/index.html')
 
-def about(request):
-    return HttpResponse('<h3 style="color: ff0000"><i>Информация о компании</i></h3>')
+def about(request):  # 2. создали метод
+    return HttpResponse('<h3 style="color: ff0000"><i>Информация о компании</i></h3>') # 3. прописываем, что возвращаем
