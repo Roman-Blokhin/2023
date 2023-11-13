084b419ca120cf2d7c631f6594eba2cabaf7df49
Roman <11-roman@mail.ru>
2023-11-13 20:11:23 +0300

    подключаем ссылки на страницы, создали папку с шаблонами и прописали сами шаблоны. Подключили все файлы друг к другу

--- django/mysite/mysite/main/urls.py
+++ django/mysite/mysite/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
+# ПОДКЛЮЧАЕМ ССЫЛКИ НА СТРАНИЦЫ
 urlpatterns = [
-    path('', include('main.urls')),
-    path('about/', include('main.urls')),
+    path('', views.main),
+    path('about/', views.about),
 ]
--- django/mysite/mysite/main/views.py
+++ django/mysite/mysite/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ФУНКЦИЯ ПОДКЛЮЧЕНИЯ СТРАНИЦЫ, здесь подключаем шаблоны, чтобы не писать код в одном месте
+
+def main(request):
+    return render (request, 'main/index.html')
+
+def about(request):
+    return render (request, 'main/about.html')
