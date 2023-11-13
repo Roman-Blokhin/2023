24f3e9a1ff474aa8cb05df63df319a961e01510e
Roman <11-roman@mail.ru>
2023-11-13 13:07:19 +0300

    Счетчик среднего значения без замыкания с помощью магического метода __call__

--- OOP/lesson_23.py
+++ OOP/lesson_23.py
@@ -12,3 +12,20 @@ class Counter:
     def __call__(self, *args, **kwargs):  # можем теперь вызывать сам элемент: a() без замыкания
         self.counter += 1
         print(f'Объект {self.name} вызывался {self.counter} раз')
+
+# --------------- Счетчик среднего значения без замыкания ---------------- #
+class Count:
+    def __init__(self):
+        self.counter = 0
+        self.summa = 0  # сумма
+        self.length = 0  # длина
+        print(f'Объект инициализирован:', self)
+
+    def __call__(self, *args, **kwargs):  # можем теперь вызывать сам элемент: a() без замыкания
+        self.counter += 1
+        self.summa = sum(args)  # так как *args сохраняет значения в tuple и там будут числа, то сумму посчитает
+        self.length = len(args)
+        print(f'Объект {self} вызывался {self.counter} раз')
+
+    def average(self):
+        return self.summa/self.length
