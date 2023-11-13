6ec56dd18c0f88fa29db8c1cbc9536aaf068b5a2
Roman <11-roman@mail.ru>
2023-11-13 12:55:52 +0300

    сделали счетчик без замыкания с помощью магического метода __call__

--- OOP/lesson_23.py
+++ OOP/lesson_23.py
@@ -1,12 +1,14 @@
 # Магический метод __call__
 
 class Counter:
-    def __init__(self, name):  # инициализация
-        self.name = name
+    def __init__(self):  # инициализация
+        self.name = 'Roman'
+        self.counter = 0
         print(f'Объект инициализирован:', self.name)
 
     def __repr__(self):  # имя объекта понятно написано
         return f'Славный объект: {self.name}'
 
-    def __call__(self, *args, **kwargs):  # можем теперь вызывать сам элемент: a()
-        print(f'Объект {self.name} вызван')
+    def __call__(self, *args, **kwargs):  # можем теперь вызывать сам элемент: a() без замыкания
+        self.counter += 1
+        print(f'Объект {self.name} вызывался {self.counter} раз')
