d0adbbbc5add2dcb4327a074045403674897389a
Roman <11-roman@mail.ru>
2023-11-13 17:56:03 +0300

    -||-

--- django/mysite/mysite/mysite/settings.py
+++ django/mysite/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',  # добавил новое приложение в список
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
