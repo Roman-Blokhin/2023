b86a4a2068b4e5e772afa15bde9314b8c8f6b091
Roman <11-roman@mail.ru>
2023-11-21 14:59:15 +0300

    Передача простых данных в шаблоны

--- django/blog/blog/main/views.py
+++ django/blog/blog/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse  # 1. произвели импорт
 
 # HttpResponse не может корректно выгрузить нам всю страницу, поэтому лучше использовать render
 def index(request): # загружаем шаблон из папки templates
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
 
 def about(request):  # 2. создали метод
     return render(request, 'main/about.html')
