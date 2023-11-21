c6be7906669d85367608fb327ade91cb888537f9
Roman <11-roman@mail.ru>
2023-11-21 15:17:58 +0300

    проверили, как можно передавать список с данными на страницу, пропуская его через цикл for в jinja

--- django/blog/blog/main/views.py
+++ django/blog/blog/main/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse  # 1. произвели импорт
 
 # HttpResponse не может корректно выгрузить нам всю страницу, поэтому лучше использовать render
 def index(request): # загружаем шаблон из папки templates
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
+    data = {  # 4. создали словарь для вызова из него данных
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)  # 3. третьим параметром выводим переменную - словарь
 
 def about(request):  # 2. создали метод
-    return render(request, 'main/about.html')
+    data = {
+        'values': ['some', '123', 'welcome']  # 5. создали список данных
+    }
+    return render(request, 'main/about.html', data)
     # return HttpResponse('<h3 style="color: ff0000"><i>Информация о компании</i></h3>') # 3.прописываем, что возвращаем
