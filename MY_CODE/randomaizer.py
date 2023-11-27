01f28c1e92c79113f92e9c3bc4dcb8dbd4cbf321
Roman <11-roman@mail.ru>
2023-11-27 21:55:30 +0300

    вторая версия игры - угадай число, с количеством попыток

--- MY_CODE/randomaizer.py
+++ MY_CODE/randomaizer.py
@@ -1,16 +1,26 @@
 import random as r
 
+computer_num = r.randint(0, 10)
+point = 3
+
 while True:
     user_num = int(input('Введите число от 0 до 10: '))
-    computer_num = r.randint(0, 10)
+    point -= 1
 
     print('Компьютер загадал: ', str(computer_num))
 
     if user_num == computer_num:
-        print(f'Вы угадали число!')
+        print(f'Вы угадали число! Поздравляем!')
+        break
     elif user_num > computer_num:
         print(f'Ваше число {user_num} больше..')
     elif user_num < computer_num:
         print(f'Ваше число {user_num} меньше....')
 
-    ret = input('Нажмите Enter')
+    print(f'У вас осталось {point} попыток')
+
+    if point == 0:
+        print('Попыток больше нет. Конец..')
+        break
+
+    ret = input('Нажмите Enter ')
