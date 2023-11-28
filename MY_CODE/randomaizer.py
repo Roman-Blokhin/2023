2c49c1e54583ef173cea467bb73f6028aae0548a
Roman <11-roman@mail.ru>
2023-11-28 08:41:17 +0300

    обработка исключений

--- MY_CODE/randomaizer.py
+++ MY_CODE/randomaizer.py
@@ -4,21 +4,28 @@ computer_num = r.randint(0, 10)
 point = 3
 
 while True:
-    user_num = int(input('Введите число от 0 до 10: '))
-    point -= 1
-
-    print('Компьютер загадал: ', str(computer_num))
+    try:
+        user_num = int(input('Введите число от 0 до 10: '))
+    except ValueError:
+        print('Попробуйте еще раз')
+        try:
+            user_num = int(input('Введите число от 0 до 10: '))
+        except ValueError:
+            print('Попробуйте еще раз')
 
     if user_num == computer_num:
         print(f'Вы угадали число! Поздравляем!')
         break
     elif user_num > computer_num:
-        print(f'Ваше число {user_num} больше..')
+        print(f'Ваше число: {user_num}, больше..')
     elif user_num < computer_num:
-        print(f'Ваше число {user_num} меньше....')
+        print(f'Ваше число: {user_num}, меньше....')
 
-    print(f'У вас осталось {point} попыток')
+    print('Компьютер загадал: ', str(computer_num))
 
+    print(f'Оставшиеся попытки: {point}')
+
+    point -= 1
     if point == 0:
         print('Попыток больше нет. Конец..')
         break
