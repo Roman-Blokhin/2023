f9e35e5c75e1e872d8889527def3b5d3ec0c0504
Roman <11-roman@mail.ru>
2023-11-30 21:35:23 +0300

    изменил кнопки и их вывод на экран

--- MY_CODE/buttons_tkinter.py
+++ MY_CODE/buttons_tkinter.py
@@ -1,12 +1,24 @@
 from tkinter import *
+import random as r
+
+
+def com_1():
+    num_1 = r.randint(0, 100)
+    Label(root, text=num_1).grid(row=2, column=1)
+
+
+def com_2():
+    num_2 = r.randint(0, 100)
+    Label(root, text=num_2).grid(row=2, column=3)
+
 
 root = Tk()
-root.geometry ('180x130+300+300')
+root.geometry('200x130+300+300')
 root.config(bg='pink')
 
-btn1 = Button(root, text='привет')
+btn1 = Button(root, text='Игрок 1', command=com_1)
 btn1.grid(row=1, column=1)
-btn2 = Button(root, text='пока')
+btn2 = Button(root, text='Игрок 2', command=com_2)
 btn2.grid(row=1, column=3)
 
 Label(root, text='', bg='pink').grid(row=0, column=0)
