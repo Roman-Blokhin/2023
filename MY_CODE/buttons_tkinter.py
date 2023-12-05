9cd13e3d1756c6c6813f7e0ecb6981614c3d706b
Roman <11-roman@mail.ru>
2023-12-05 20:13:17 +0300

    изменил параметры наших объектов программы

--- MY_CODE/buttons_tkinter.py
+++ MY_CODE/buttons_tkinter.py
@@ -12,32 +12,38 @@ def com_1():
     win_2.insert(END, num_2)
 
     if num_1 > num_2:
-        Label(root, text='ПЕРВЫЙ ИГРОК ПОБЕДИЛ').grid(row=4, column=1, columnspan=5)
+        Label(root, text='ПЕРВЫЙ ИГРОК ПОБЕДИЛ', font=("Comic Sans MS", 12, "normal"), bg='pink').\
+            grid(row=4, column=1, columnspan=4)
     elif num_1 == num_2:
-        Label(root, text='НИЧЬЯ').grid(row=4, column=1, columnspan=5)
+        Label(root, text='                  НИЧЬЯ                 ', font=("Comic Sans MS", 12, "normal"), bg='pink').\
+            grid(row=4, column=1, columnspan=4)
     else:
-        Label(root, text='ВТОРОЙ ИГРОК ПОБЕДИЛ').grid(row=4, column=1, columnspan=5)
+        Label(root, text='ВТОРОЙ ИГРОК ПОБЕДИЛ', font=("Comic Sans MS", 12, "normal"), bg='pink').\
+            grid(row=4, column=1, columnspan=4)
 
 
 root = Tk()
-root.geometry('200x130+300+300')
+root.geometry('500x530+300+300')
 root.config(bg='pink')
 
-btn1 = Button(root, text='Старт', command=com_1)
-btn1.grid(row=1, column=1, columnspan=3)
+btn1 = Button(root, text='Старт', font=("Comic Sans MS", 12, "normal"), command=com_1)
+btn1.grid(row=1, column=1, columnspan=5)
 
-Label(root, text='', bg='pink').grid(row=0, column=0)
-Label(root, text='', bg='pink').grid(row=0, column=2)
-Label(root, text='', bg='pink').grid(row=2, column=2)
+Label(root, text='', bg='pink', font=("Comic Sans MS", 12, "normal")).grid(row=0, column=0)
+Label(root, text='', bg='pink', font=("Comic Sans MS", 12, "normal")).grid(row=0, column=2)
+Label(root, text='', bg='pink', font=("Comic Sans MS", 12, "normal")).grid(row=2, column=2)
 
-win = Entry(root, width=3)
+win = Entry(root, width=3, font=("Comic Sans MS", 12, "normal"))
 win.grid(row=3, column=1)
 
-win_2 = Entry(root, width=3)
+win_2 = Entry(root, width=3, font=("Comic Sans MS", 12, "normal"))
 win_2.grid(row=3, column=3)
 
 root.grid_rowconfigure(0, minsize=30)
-root.grid_columnconfigure(0, minsize=30)
-root.grid_columnconfigure(2, minsize=30)
+root.grid_rowconfigure(1, minsize=30)
+root.grid_rowconfigure(2, minsize=30)
+root.grid_columnconfigure(0, minsize=100)
+root.grid_columnconfigure(1, minsize=100)
+root.grid_columnconfigure(2, minsize=100)
 
 root.mainloop()
