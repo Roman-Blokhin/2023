313dc162c265343c8f86924a248cc85c3d77dd56
Roman <11-roman@mail.ru>
2023-12-12 19:34:27 +0300

    создали контейнер и поле для текста с базовыми параметрами

--- MY_CODE/notebook/main.py
+++ MY_CODE/notebook/main.py
@@ -1,8 +1,16 @@
 from tkinter import *
 
-root = Tk ()
-root.title ('Блокнот')
-root.geometry ('650x450+200+200')
+# 1. создали основу для программы, окно
+
+root = Tk()
+root.title('Блокнот')
+root.geometry('650x450+200+200')
 root.iconbitmap(default='logo.ico')
 
-root.mainloop ()
+f_text = Frame(root)  # 2. создали контейнер для текстового поля
+f_text.pack(fill=BOTH, expand=1)  # 3. fill=BOTH - растяг. по сторонам х и у, expand=1 - виджет заполн. все простр.
+
+text_fild = Text(f_text, bg='black', fg='lime')
+text_fild.pack(fill=BOTH, expand=1, side=LEFT)  # 3. side=LEFT - выравнивание по левой стороне контейнера
+
+root.mainloop()
