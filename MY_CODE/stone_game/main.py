592fcb208f032bff9396093b31b60581f92fb37d
Roman <11-roman@mail.ru>
2023-12-15 22:52:44 +0300

    при выборе варианта пользователем, на экране появляется рандомный вариант компьютера

--- MY_CODE/stone_game/main.py
+++ MY_CODE/stone_game/main.py
@@ -1,18 +1,21 @@
 from tkinter import *
+import random as r
 
 
+# функция выбора варианта компьютером из списка значений и вывод результата
 def starting():
-    pass
+    ssp = ('Камень', 'Ножницы', 'Бумага')
+    value = r.choice(ssp)  # рандомный выбор из списка
+    comp_var.config(text=value)  # меняем параметр text у comp_var
 
 
-root = Tk ()
-root.title ('Камень ножницы бумага')
-root.geometry ('450x350+200+200')
-root.config (bg = 'black')
+root = Tk()
+root.title('Камень ножницы бумага')
+root.geometry('450x350+200+200')
+root.config(bg='black')
 root.resizable(width=False, height=False)
 
-values = ['Камень', 'Ножницы', 'Бумага']
-
+# кнопки для выбора варианта пользователем
 stone = Button(root, text='Камень', font=('Comic Sans MS', 14), command=starting)
 stone.place(x=40, y=200)
 
@@ -22,5 +25,8 @@ scissors.place(x=170, y=200)
 paper = Button(root, text='Бумага', font=('Comic Sans MS', 14), command=starting)
 paper.place(x=330, y=200)
 
+# рандомный вариант компьютера
+comp_var = Label(root, text='', font=('Comic Sans MS', 14), fg='white', bg='black')
+comp_var.place(x=180, y=100)
 
-root.mainloop ()
+root.mainloop()
